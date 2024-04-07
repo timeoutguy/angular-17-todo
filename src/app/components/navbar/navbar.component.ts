0ee259b2c9ea7ffbadaf98b17d727e49371c8a95
Marcus Vinícius <marcus.dev@protonmail.com>
2024-04-07 14:58:46 -0300

    feat: navbar change based on user.token state

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { UserStore } from '../../stores/user.store';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  public userStore = inject(UserStore);
 }
