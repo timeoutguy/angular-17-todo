dc622330fe09adc4fd4e3964b3c828643741ee71
Marcus Vinícius <marcus.dev@protonmail.com>
2024-04-07 14:58:25 -0300

    feat: create routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'register', component: RegisterComponent }
+];
