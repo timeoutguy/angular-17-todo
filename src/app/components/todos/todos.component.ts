0a75824a5620451fcadddbc6b7da03d86c666823
Marcus Vinícius <marcus.dev@protonmail.com>
2024-04-07 21:10:48 -0300

    feat: skeleton loading in todos component

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { TodosStore } from '../../stores/todos.store';
 import { TodoCardComponent } from '../todo-card/todo-card.component';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [CommonModule, TodoCardComponent],
+  imports: [CommonModule, TodoCardComponent, NgxSkeletonLoaderModule],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss',
 })
